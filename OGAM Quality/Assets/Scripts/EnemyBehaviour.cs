3d78c6fb56ed98e3d53c0ce2bc16f310aad076f5
Dahl <Fatalgnome@hotmail.dk>
2018-04-04 18:03:26 +0200

    Added player movement, Cinemachine and an enemy
    Enemy is still not done

---
+++ OGAM Quality/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    private int health;
+
+    public float Speed;
+
+    private Vector2 direction;
+
+    private int random;
+	// Use this for initialization
+	void Start ()
+	{
+	    random = Random.Range(0, 1);
+	    if (random == 0)
+	    {
+	        direction = Vector2.left;
+	    }
+	    else
+	    {
+	        direction = Vector2.right;
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {  
+		transform.Translate(direction * Speed * Time.deltaTime);
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            if(direction.x == -1)
+            {
+                direction.x = 1;
+            }
+            else if (direction.x == 1)
+            {
+                direction.x = -1;
+            }
+
+        }
+    }
+}
---
+++ OGAM Quality/Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float curSpeed;
+    private Vector2 movDir;
+    private Rigidbody2D rigidbody;
+
+    public LayerMask groundLayer;
+    public float Speed = 3.0f;
+    public float jumpSpeed;
+    public int Health;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            movDir += Vector2.left;
+            transform.Translate(movDir * Speed * Time.deltaTime, Camera.main.transform);
+        }
+        else
+        {
+            movDir = transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            movDir += Vector2.right;
+            transform.Translate(movDir * Speed * Time.deltaTime, Camera.main.transform);
+        }
+        else
+        {
+            movDir = transform.forward;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1, groundLayer);
+        RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(transform.position.x - 0.5f, transform.position.y), Vector2.down, 1, groundLayer);
+        RaycastHit2D hit3 = Physics2D.Raycast(new Vector2(transform.position.x + 0.5f, transform.position.y), Vector2.down, 1, groundLayer);
+
+        if (hit.collider != null || hit2.collider != null || hit3.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void LoseHealth(int dmg)
+    {
+        Health -= dmg;
+    }
+
+}
